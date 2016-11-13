754353fded051c268d8c4d737f8e6cdccc25690c
reindels <reindels@oregonstate.edu>
2016-11-13 12:20:46 -0800

    Added a the start of a menu based on a youtube tutorial.

---
+++ Menu Project/Assets/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	public Menu CurrentMenu;
+
+	// Use this for initialization
+	public void Start () {
+		ShowMenu (CurrentMenu);
+	}
+	
+	// Update is called once per frame
+	public void ShowMenu (Menu menu) {
+		if (CurrentMenu != null) {
+			CurrentMenu.IsOpen = false;
+		} 
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;
+	
+	}
+}
